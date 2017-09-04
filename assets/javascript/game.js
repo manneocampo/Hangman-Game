54d49dcd11247ca8ce00ab093f338de772f05227
Michelle Ocampo <manneocampo@gmail.com>
2017-09-03 20:28:29 -0400

    Added changes to index and js

---
+++ assets/javascript/game.js
@@ -0,0 +1,5 @@
+//Word selector array 
+var words = ["Hermione", "Harry", "Ron", "Snape", "Draco","Dumbledore","Voldemort", "Muggles", "Horcrux", "Elder Wand", "Alohomora", "Avada Kedavra"];
+
+//Making some test logs.
+console.log("First statement");
