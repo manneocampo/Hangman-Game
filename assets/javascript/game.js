87939fab6c6a9213a23b85d43c9003a2e01ee956
Michelle Ocampo <manneocampo@gmail.com>
2017-09-03 23:00:45 -0400

    Added changes to js and index

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,43 @@
 //Word selector array 
-var words = ["Hermione", "Harry", "Ron", "Snape", "Draco","Dumbledore","Voldemort", "Muggles", "Horcrux", "Elder Wand", "Alohomora", "Avada Kedavra"];
+var words = ["hermione", "harry", "ron", "snape", "draco","dumbledore","voldemort", "muggles", "horcrux", "elder wand", "alohomora", "avada kedavra"];
+
+var word = words[Math.floor(Math.random()*words.length)];
+
+var answerArray = [];
+
+for (var i=0; i<word.length; i++){
+	answerArray[i]="_";
+}
+var remainingLetters=word.length;
 
 //Making some test logs.
-console.log("First statement");
+console.log("Hermione");
 console.log(words[11]);
+
+//Looping through each item in the array to create current word 
+//and logging a message to the console. 
+// for (var i=0; i < words.length; i++) {
+// 	console.log("Word Selected is..." + words[i]);
+// }
+
+//guess
+var guess; 
+
+//stored guesses 
+var guesses = [];
+
+//Guesses remaining 
+var guessesRemaining;
+
+//Win Points counter 
+var winPoints; 
+
+//When any letter is clicked 
+var lettersGuessed = document.getElementById("lettersGuessed"); 
+
+document.onKeyUp = function(event){
+	lettersGuessed.textContent = event.key; 
+};
+
+
+
